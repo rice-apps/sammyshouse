5f1e4f4c4f9676467ad6e9c844162cae1b59fc76
CarsonFoster <carsonwfoster@gmail.com>
2022-10-22 14:02:43 -0500

    Pull out necessary info from Date

--- app/EventCard.tsx
+++ app/EventCard.tsx
@@ -4,7 +4,13 @@ import EventData from './EventData';
 const EventCard = (props: {
     data: EventData,
 }) => {
-
+    const month: string = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(props.data.dateTime);
+    const weekday: string = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(props.data.dateTime).toUpperCase();
+    const dayOfMonth: string = props.data.dateTime.getDate().toString();
+    const time: string = props.data.dateTime.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
 };
 
 export default EventCard;
