f0600472beecc85e02e03da66d80057426c066fd
CarsonFoster <carsonwfoster@gmail.com>
2022-10-22 14:21:29 -0500

    Add basic structure of card

--- app/EventCard/EventCard.tsx
+++ app/EventCard/EventCard.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import EventData from './EventData';
 
 const defaultImageURI: string = 'https://brand.rice.edu/sites/g/files/bxs2591/files/2019-08/190308_Rice_Mechanical_Brand_Standards_Logos-2.png';
+
+const styles = StyleSheet.create({
+    mainImage: {
+
+    },
+    icon: {
+
+    }
+});
 
 const EventCard = (props: {
     data: EventData,
@@ -17,6 +27,29 @@ const EventCard = (props: {
     if (props.data.imageURI == undefined) {
         props.data.imageURI = defaultImageURI;
     }
+
+    return (
+        <View>
+            <Image source={{ uri: props.data.imageURI }} style={styles.mainImage} />
+            <View>
+                <Text>{props.data.eventName}</Text>
+                <Text>{props.data.postedBy}</Text>
+                <View>
+                    <Image source={require('@expo/snack-static/react-native-logo.png')} style={styles.icon}/>
+                    <Text>{time}</Text>
+                </View>
+                <View>
+                    <Image source={require('@expo/snack-static/react-native-logo.png')} style={styles.icon}/>
+                    <Text>{props.data.location}</Text>
+                </View>
+            </View>
+            <View>
+                <Text>{month}</Text>
+                <Text>{dayOfMonth}</Text>
+                <Text>{weekday}</Text>
+            </View>
+        </View>
+    );
 };
 
 export default EventCard;
