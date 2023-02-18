a38e23847d968f9e7ee8a245b7c95023dae2978d
Allen Dong <allendong2003@gmail.com>
2023-02-18 14:31:49 -0600

    temp prof page

--- app/pages/ProfilePage.tsx
+++ app/pages/ProfilePage.tsx
@@ -1,37 +1,51 @@
 import React, { Component, FC, useState } from 'react';
-import {  Divider, Flex, Box, Avatar, ScrollView, VStack, Center, useTheme, Heading, NativeBaseProvider } from "native-base";
-import { Provider as PaperProvider } from 'react-native-paper';
+import { View } from 'react-native';
+import {  Divider, Flex, Box, Avatar, HStack, ScrollView, VStack, Center, Heading, NativeBaseProvider, Text} from "native-base";
 
-// notes: style the avatar to the left, implement functionality to event list. 
-
-export default function ProfilePage() {
-
-    return (
-        <PaperProvider>
-          <Avatar bg="green.500" source={{
-          uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"}}>
-          </Avatar>
-          <Box alignItems="center">
-            <Flex mx="3" direction="row" justify="space-evenly" h="60">
-                <Heading py="2">Friends</Heading>
-                <Divider orientation="vertical" mx="3" _light={{
+const ProfilePage = () => {
+  return (
+    <View>
+      <HStack justifyContent="center" space={2}>
+      <Avatar 
+          title = "Allen"
+          bg="green.500" 
+          source={{
+          uri: "https://picsum.photos/700"}}>
+      </Avatar>
+      </HStack>
+      
+      <Text variant="bodyMedium"> BIO </Text>
+      <Box alignItems="center">
+            <Flex mx="3" direction="row" justify="space-evenly" h="30">
+                <Heading py="3" fontSize="s">    Friends          </Heading>
+                <Divider orientation="vertical" mx="5" _light={{
                 bg: "muted.800"}} _dark={{
                 bg: "muted.50"}} />
-                <Heading py="2">Organizations</Heading>
+                <Heading py="3"  fontSize="s" >Organizations  </Heading>
             </Flex>
-          </Box>
-          <ScrollView w={["200", "300"]} h="80">
+        </Box>
+        <ScrollView w={["300", "300"]} h="80">
           <Center mt="3" mb="4">
             <Heading fontSize="xl">Liked Events</Heading>
           </Center>
-          <VStack space={4} alignItems="center">
-            <Center w="64" h="20" bg="indigo.300" rounded="md" shadow={3} text ="placeholder"/>
-            <Center w="64" h="20" bg="indigo.500" rounded="md" shadow={3} />
-            <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3} />
-            <Center w="64" h="20" bg="indigo.300" rounded="md" shadow={3} />
-            <Center w="64" h="20" bg="indigo.500" rounded="md" shadow={3} />
-            <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3} />
+          <VStack space={4}>
+            <Center w="80" h="20" bg="indigo.300" rounded="md" shadow={3} text ="placeholder"/>
+            <Center w="80" h="20" bg="indigo.500" rounded="md" shadow={3} />
+            <Center w="80" h="20" bg="indigo.700" rounded="md" shadow={3} />
+            <Center w="80" h="20" bg="indigo.300" rounded="md" shadow={3} />
           </VStack>
           </ScrollView>
-        </PaperProvider>);
+      </View>
+      );
+};
+
+    export default () => {
+        return (
+          <NativeBaseProvider>
+            <Center flex={1} px="3">
+                <ProfilePage />
+            </Center>
+          </NativeBaseProvider>
+        );
     };
+    
